5b5cec85986528d21eeb289791d50f9440a6a90a
Alex Curtis <83910518+tripalc@users.noreply.github.com>
2023-09-27 19:28:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+def main():
+    print("The Forest - Automatic Log Duplication")
+    print("Open The Forest in 3 seconds...")
+
+    time.sleep(3)
+
+    while True:
+        pyautogui.press('e')
+        pyautogui.press('c')
+        pyautogui.press('g')
+
+if __name__ == "__main__":
+    main()
